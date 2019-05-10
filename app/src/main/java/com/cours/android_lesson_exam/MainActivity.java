09d4fcea3b12199870031beb45d2419de4ae906d
Cyril Challouatte <you@example.com>
2019-05-10 15:47:47 +0200

    rendu

--- app/src/main/java/com/cours/android_lesson_exam/MainActivity.java
+++ app/src/main/java/com/cours/android_lesson_exam/MainActivity.java
@@ -1,19 +1,29 @@
 package com.cours.android_lesson_exam;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.cours.android_lesson_exam.datas.Contact;
 import com.cours.android_lesson_exam.datas.ContactViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
+    private ContactViewModel contactViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.add);
         //TODO ajout des listeners
 
+        fab.setOnClickListener(onClickListener);
+        listView.setOnItemClickListener(onItemClickListener);
+        listView.setOnItemLongClickListener(onItemLongClickListener);
         //Mise à jour de la liste
         updateList();
     }
@@ -32,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             //TODO démarrage de l'activité d'édition
+            Intent intent = new Intent(MainActivity.this, EditActivity.class);
+            startActivity(intent);
         }
     };
 
@@ -39,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             //TODO démarrer l'activité d'édition en passant en paramètre l'id du contact sélectionné (Intent.EXTRA_UID)
+            Intent intent = new Intent(MainActivity.this, EditActivity.class);
+            Contact contact = (Contact) parent.getItemAtPosition(position);
+            intent.putExtra(Intent.EXTRA_UID, contact.getId());
+            startActivity(intent);
         }
     };
 
@@ -46,11 +65,33 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
             //TODO gérer la suppression avec confirmation
-            return false;
+            final AdapterView<?> finalParent = parent;
+            final int finalPosition = position;
+
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.delete_title)
+                    .setMessage(R.string.delete_message)
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Contact contact = (Contact) finalParent.getItemAtPosition(finalPosition);
+                            contactViewModel.delete(contact);
+                        }
+                    }).setNegativeButton(R.string.no, null).show();
+            return true;
         }
     };
 
     private void updateList() {
         //TODO Mettre à jour la liste avec le contenu de la base de donnée
+        final MainActivity mainActivity = new MainActivity();
+        contactViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
+        contactViewModel.getAll().observe(this, new Observer<List<Contact>>() {
+            @Override
+            public void onChanged(List<Contact> contacts) {
+                ContactAdapter contactAdapter = new ContactAdapter(MainActivity.this, contacts);
+                listView.setAdapter(contactAdapter);
+            }
+        });
     }
 }
--- app/src/main/java/com/cours/android_lesson_exam/datas/ContactViewModel.java
+++ app/src/main/java/com/cours/android_lesson_exam/datas/ContactViewModel.java
@@ -1,14 +1,75 @@
 package com.cours.android_lesson_exam.datas;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
 
 public class ContactViewModel extends AndroidViewModel {
+    private ContactDAO contactDAO;
+
     public ContactViewModel(@NonNull Application application) {
         super(application);
+
+        contactDAO = Database.getInstance(application.getApplicationContext()).contactDAO();
     }
 
     //TODO ajouter le contenu
+
+    public LiveData<List<Contact>> getAll() {
+        return contactDAO.getAll();
+    }
+
+    public LiveData<Contact> getById(int id) {
+        return contactDAO.getById(id);
+    }
+
+    public void insertOrUpdateContact(Contact contact) {
+        new InsertOrUpdateTask(contactDAO).execute(contact);
+    }
+
+    private static class InsertOrUpdateTask extends AsyncTask<Contact, Void, Void>{
+        private ContactDAO contactDAO;
+
+        public InsertOrUpdateTask(ContactDAO contactDAO){
+            this.contactDAO = contactDAO;
+        }
+
+        @Override
+        protected Void doInBackground(Contact... contacts){
+            Contact contact = contacts[0];
+            if(contact.getId() > 0){
+                contactDAO.update(contact);
+            }else{
+                contactDAO.insert(contact);
+            }
+
+            return null;
+        }
+    }
+
+    public void delete(Contact contact) {
+        new DeleteTask(contactDAO).execute(contact);
+    }
+
+
+    private static class DeleteTask extends AsyncTask<Contact, Void, Void> {
+        private ContactDAO contactDAO;
+
+        public DeleteTask(ContactDAO contactDAO) {
+            this.contactDAO = contactDAO;
+        }
+
+        @Override
+        protected Void doInBackground(Contact... contacts) {
+            Contact contact = contacts[0];
+            contactDAO.delete(contact);
+            return null;
+        }
+    }
+
 }
